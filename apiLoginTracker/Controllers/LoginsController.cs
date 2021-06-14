0a7fdb948b92f4f433f747a0f2d18ffe72489972
Rich Eodice <rich@eodice.com>
2021-06-13 21:06:26 -0400

    1st build

---
+++ apiLoginTracker/Controllers/LoginsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1apiLoginTracker.Models;
+using MongoDB.Driver;
+using apiLoginTracker;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1apiLoginTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginsController : ControllerBase
+    {
+        // GET: api/<LoginsController>
+        [HttpGet]
+        public IEnumerable<Login> Get(int skip = 0, int take = 10)
+        {
+            try
+            {
+                var dbClient = new MongoClient(configuration.dbconnection);
+                var database = dbClient.GetDatabase(configuration.dbname);
+                var collection = database.GetCollection<Login>(configuration.dbcollection);
+
+                var result = collection.AsQueryable()
+                    .OrderByDescending(l => l.Timestamp)
+                    .Skip(skip)
+                    .Take(take)
+                    .ToList();
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        // GET api/<LoginsController>/5
+        [HttpGet("{id}")]
+        public Login Get(Guid id)
+        {
+            try
+            {
+                var dbClient = new MongoClient(configuration.dbconnection);
+                var database = dbClient.GetDatabase(configuration.dbname);
+                var collection = database.GetCollection<Login>(configuration.dbcollection);
+
+                var result = collection.AsQueryable()
+                    .FirstOrDefault(l => l.Id == id);
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+               
+    }
+}
---
+++ apiLoginTracker/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebApplication1apiLoginTracker.Models
+{
+    public class Login
+    {
+        public Guid Id { get; set; }
+        public string LoginType { get; set; }
+        public string Username { get; set; }
+        public string Machine { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string DomainController { get; set; }
+        public string Gateway { get; set; }
+    }
+}
